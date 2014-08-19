69adbeb43805097b404dc89323240f4ae12a3c17
Adam Magaluk <AdamMagaluK@gmail.com>
2014-08-19 14:13:02 -0400

    Scout now inherits from autoscout

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+var util = require('util');
 var AutoScout = require('zetta-auto-scout');
 var Buzzer = require('./buzzer_driver');
 
-module.exports = new AutoScout('buzzer', Buzzer);
+var BoneScout = module.exports = function(pin) {
+  AutoScout.call(this, 'buzzer', Buzzer, pin);
+};
+util.inherits(BoneScout, AutoScout);
